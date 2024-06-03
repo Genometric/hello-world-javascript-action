024829b72b9b5ef538c513123e36a4b81541a87a
VJalili <VJalili@users.noreply.github.com>
2024-06-03 19:43:18 -0400

    udpate.

--- index.js
+++ index.js
@@ -3,47 +3,50 @@ const github = require('@actions/github');
 const { BlobServiceClient } = require("@azure/storage-blob");
 const { DefaultAzureCredential } = require('@azure/identity');
 
+
+async function uploadBlob() {
+  try {
+    const accountName = core.getInput('azure-storage-account-name');
+    // process.env.AZURE_STORAGE_ACCOUNT_NAME;
+    if (!accountName) throw Error('Azure Storage accountName not found');
+
+    const blobServiceClient = new BlobServiceClient(
+    `https://${accountName}.blob.core.windows.net`,
+    new DefaultAzureCredential()
+    );
+
+    const containerName = core.getInput('azure-storage-container-name'); // 'quickstart';
+
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+
+    // Create a unique name for the blob
+    const blobName = core.getInput('azure-storage-blob-name'); // 'quickstart.txt';
+
+    // Get a block blob client
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+
+    // Display blob name and url
+    console.log(
+    `\nUploading to Azure storage as blob\n\tname: ${blobName}:\n\tURL: ${blockBlobClient.url}`
+    );
+
+    // Upload data to the blob
+    const data = 'Hello, World!';
+    const uploadBlobResponse = await blockBlobClient.upload(data, data.length);
+    console.log(
+    `Blob was uploaded successfully. requestId: ${uploadBlobResponse.requestId}`
+    );
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
 
-  const accountName = core.getInput('azure-storage-account-name');
-  // process.env.AZURE_STORAGE_ACCOUNT_NAME;
-  if (!accountName) throw Error('Azure Storage accountName not found');
-
-  const blobServiceClient = new BlobServiceClient(
-    `https://${accountName}.blob.core.windows.net`,
-    new DefaultAzureCredential()
-  );
-
-  const containerName = core.getInput('azure-storage-container-name'); // 'quickstart';
-
-  const containerClient = blobServiceClient.getContainerClient(containerName);
-
-//
-//  // Create a unique name for the blob
-//  const blobName = 'quickstart.txt';
-//  console.log('----5');
-//
-//  // Get a block blob client
-//  const blockBlobClient = containerClient.getBlockBlobClient(blobName);
-//  console.log('----6');
-//
-//  // Display blob name and url
-//  console.log(
-//    `\nUploading to Azure storage as blob\n\tname: ${blobName}:\n\tURL: ${blockBlobClient.url}`
-//  );
-//
-//  console.log('----7');
-//
-//  // Upload data to the blob
-//  const data = 'Hello, World!';
-//  console.log('----8');
-//  const uploadBlobResponse = await blockBlobClient.upload(data, data.length);
-//  console.log(
-//    `Blob was uploaded successfully. requestId: ${uploadBlobResponse.requestId}`
-//  );
+  uploadBlob();
 
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
