219f829c5d28821c0b0526df7e619c169fbde53d
VJalili <VJalili@users.noreply.github.com>
2024-06-03 18:38:41 -0400

    udpate.

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { DefaultAzureCredential } = require('@azure/identity');
 
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
+
+  const accountName = process.env.AZURE_STORAGE_ACCOUNT_NAME;
+  if (!accountName) throw Error('Azure Storage accountName not found');
+
+  const blobServiceClient = new BlobServiceClient(
+    `https://${accountName}.blob.core.windows.net`,
+    new DefaultAzureCredential()
+  );
+  const containerName = 'quickstart';
+  const containerClient = blobServiceClient.getContainerClient(containerName);
+
+  // Create a unique name for the blob
+  const blobName = 'quickstart.txt';
+
+  // Get a block blob client
+  const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+
+  // Display blob name and url
+  console.log(
+    `\nUploading to Azure storage as blob\n\tname: ${blobName}:\n\tURL: ${blockBlobClient.url}`
+  );
+
+  // Upload data to the blob
+  const data = 'Hello, World!';
+  const uploadBlobResponse = await blockBlobClient.upload(data, data.length);
+  console.log(
+    `Blob was uploaded successfully. requestId: ${uploadBlobResponse.requestId}`
+  );
+
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
