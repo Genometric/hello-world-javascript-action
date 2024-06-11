69c0f0575beea869fc6a7dadbf0c2ee19f65c406
Matt McLoughlin <mattmcl@microsoft.com>
2024-06-11 10:30:45 -0700

    simplify logic

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { BlobServiceClient } = require("@azure/storage-blob");
 const { DefaultAzureCredential } = require('@azure/identity');
 const { v4: uuidv4 } = require('uuid');
 
-async function createTriggerFile() {
+async function runWorkflowAsync() {
     const accountName = core.getInput('azure-storage-account-name');
     if (!accountName) {
         core.setFailed('Azure Storage accountName not found');
@@ -43,29 +43,23 @@ async function createTriggerFile() {
 
     // Loop to check for success or failure indicators
     while (true) {
-        let successFound = false;
-        let failureFound = false;
-
-        for await (const blob of containerClient.listBlobsFlat({ prefix: succeededBlobPrefix })) {
-            if (blob.name.startsWith(succeededBlobPrefix)) {
-                successFound = true;
-                break;
+        try {
+            for await (const blob of containerClient.listBlobsFlat({ prefix: succeededBlobPrefix })) {
+                if (blob.name.startsWith(succeededBlobPrefix)) {
+                    console.log('Workflow succeeded.');
+                    core.setOutput("status", "succeeded");
+                    break;
+                }
             }
-        }
-        for await (const blob of containerClient.listBlobsFlat({ prefix: failedBlobPrefix })) {
-            if (blob.name.startsWith(failedBlobPrefix)) {
-                failureFound = true;
-                break;
-            }
-        }
 
-        if (successFound) {
-            console.log('Workflow succeeded.');
-            core.setOutput("status", "succeeded");
-            break;
-        } else if (failureFound) {
-            core.setFailed('Workflow failed.');
-            break;
+            for await (const blob of containerClient.listBlobsFlat({ prefix: failedBlobPrefix })) {
+                if (blob.name.startsWith(failedBlobPrefix)) {
+                    core.setFailed('Workflow failed.');
+                    break;
+                }
+            }
+        } catch (error) {
+            console.log(`Error occurred; will retry in 30s.  ${error.message}`);
         }
 
         await new Promise(resolve => setTimeout(resolve, 30000)); // wait 30 seconds before checking again
@@ -74,7 +68,7 @@ async function createTriggerFile() {
 
 async function run() {
     try {
-        await createTriggerFile();
+        await runWorkflowAsync();
         const time = (new Date()).toTimeString();
         core.setOutput("time", time);
         // Get the JSON webhook payload for the event that triggered the workflow
